4618290bd4765b6d81fdc99a21111bb29fe0b132
jbee.han <jbee.han@navercorp.com>
2018-01-05 16:02:45 +0900

    chage folder name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import Controller from "../src/ts/Controller";
-import Controller from "../src/observer_pattern/Controller";
+import Controller from "../src/tutorial_code/Controller";
+// import Controller from "../src/observer_pattern/Controller";
 
 (() => new Controller())();
